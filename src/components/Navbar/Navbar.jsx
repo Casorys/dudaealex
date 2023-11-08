4e7e95302a3fcb55ad7d2140cd719d5aa1350cc6
Tiago de Freitas <tiagofreitas023023@gmail.com>
2023-11-08 16:41:12 -0300

    v1.1.2
    Added a collapsable navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
 import React, { useRef } from 'react'
 import { useState, useEffect } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './Navbar.css'
 
 const Navbar = () => {
 
   const [navbar,setNavbar] = useState(false)
+  const [menuColor, setMenuColor] = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const Menu = () => (
+    <>
+    <a href="#programacao" className={navbar ? 'presentesLinkActive' : 'presentesLink'}>Programação</a>
+          <a href="#dicas" className={navbar ? 'presentesLinkActive' : 'presentesLink'}>Dicas</a>
+          <p className='buttonPresenca'><a href='#rsvp'>Confirmar Presença</a></p>
+    </>
+  )
 
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
       setNavbar(true)
+      setMenuColor(true)
     } 
     else {
       setNavbar(false)
+      setMenuColor(false)
     }
   }
 
@@ -21,9 +34,17 @@ const Navbar = () => {
     <div className={navbar ? 'navbarMainContainerActive' : 'navbarMainContainer'}>
         <a href="#top" className={navbar ? 'deaTextActive' : 'deaText'}>D&A</a>
         <div className='pagesContainer'>
-          <a href="#programacao" className={navbar ? 'presentesLinkActive' : 'presentesLink'}>Programação</a>
-          <a href="#dicas" className={navbar ? 'presentesLinkActive' : 'presentesLink'}>Dicas</a>
-          <p className='buttonPresenca'><a href='#rsvp'>Confirmar Presença</a></p>
+          <Menu/>
+        </div>
+        <div className='navbarMenu'>
+          {toggleMenu ? <RiCloseLine className={menuColor ? 'menuColorActive' : 'menuColor'} onClick={() => setToggleMenu(false)}/> : <RiMenu3Line className={menuColor ? 'menuColorActive' : 'menuColor'} onClick={() => setToggleMenu(true)} />}
+          {toggleMenu && (
+            <div className='navbarMenuContainer'>
+              <div className='navbarMenuContainerLinks'>
+                <Menu/>
+              </div>
+            </div>
+          )}
         </div>
     </div>
   )
